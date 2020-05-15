b8d1672f7e00fad56a0b64f840ea5919e72ee5bc
pyroll <ecall22@gmail.com>
2020-05-15 13:28:12 -0500

    slightly modified

--- prep_all_data.py
+++ prep_all_data.py
@@ -1,4 +1,6 @@
+# Run this script to run all scripts that deal with preparing files for the main script
+
 import filesPreprocessor
 import jsonPreprocessor
 import pars_to_yaml
-import ene_clas
+# import ene_clas
